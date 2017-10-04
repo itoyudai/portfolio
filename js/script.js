406d520337d19993480fb2865a458a2e58c8fbba
itoyudai <32475461+itoyudai@users.noreply.github.com>
2017-10-04 16:49:43 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+/*	$(window).load(function(){
+		var $deferredAnim = $.Deferred( function( deferredAnim ){
+			deferredAnim.then(anim_01)
+			.then(anim_02)
+			.then(anim_03);
+		});
+		$deferredAnim.resolve();
+		function anim_01(){
+			return $('#ttl p').fadeIn(2000);
+		}
+		function anim_02(){
+			return $('#ttl p').fadeOut(2000);
+		}
+		function anim_03(){
+			return $('#ttl_wrap').fadeOut(2000);
+		}
+	});*/
+});
